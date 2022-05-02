89f55620889df06cf3b3f821c40ecb92666dcff4
rifxtt <dc.wang840533@gmail.com>
2022-05-02 12:10:04 -0400

    merge

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,13 @@
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
 import Graph from '../components/Graph'
+import Form from '../components/Form'
 
 export default function Home() {
   return (
     <Layout>
     <Graph></Graph>
+
     </Layout> 
   )
 }
