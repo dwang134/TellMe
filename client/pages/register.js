850da0191b8eefbaa2ff322366707c75d0cb2e1c
rifxtt <dc.wang840533@gmail.com>
2022-05-20 00:51:53 -0400

    register

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,10 +1,21 @@
 import Layout from '../components/Layout'
+import {useState} from 'react'
 
 const register = () => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    })
+
+    const {name, email, password, password2} = formData;
+
   return (
     <Layout>
-      <div class="flex items-center justify-center py-10">
-    <div class="px-8 py-6 mx-4 mt-4 text-left drop-shadow-md md:w-1/3 lg:w-1/3 sm:w-1/3">
+      <div class="min-h-full flex items-center justify-center py-20 px-4 sm:px-6 lg:px-8 ">
+    <div class="px-8 py-6 mx-4 mt-4 text-left drop-shadow-md">
         <div class="flex justify-center">
         <img
               class="mx-auto h-12 w-auto"
@@ -25,11 +36,11 @@ const register = () => {
                             <input type="password" placeholder="Password"
                                 class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
                 </div>
-                {/* <div class="mt-4">
+                <div class="mt-4">
                     <label class="block">Confirm Password</label>
                             <input type="password" placeholder="Password"
                                 class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
-                </div> */}
+                </div>
                 {/* <span class="text-xs text-red-400">Password must be same!</span> */}
                 <div class="flex pt-2">
                     <button class="w-full px-6 py-2 mt-4 text-white bg-indigo-600 rounded-lg hover:bg-indigo-500">Create
